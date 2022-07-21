d8e16f220b42892b68a2121f3aa7eb68d0bd815a
savoryboi <andbjerk@yahoo.com>
2022-07-21 16:28:42 -0500

    working prompts again

--- index.js
+++ index.js
@@ -1,54 +1,64 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { listenerCount } = require('process');
+
+// import classes from lib folder
+const { Manager, managerPrompts} = require('./lib/Manager');
+const { Engineer, engPrompts } = require('./lib/Engineer');
+const { Intern, internPrompts } = require('./lib/Intern');
+
+const teamArray = [];
 
 
-const menu = {
+const start = () => { managerQs() };
+
+const managerQs = () => {
+    inquirer.prompt(managerPrompts)
+    .then(answers => {
+        let manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber)
+        teamArray.push(manager);
+        return newEmployee();
+    })
+}
+
+
+const engineerQs = () => {
+    inquirer.prompt(engPrompts)
+    .then(answers => {
+        let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+        teamArray.push(engineer);
+        return newEmployee();
+    })
+}
+
+const internQs = () => {
+    inquirer.prompt(internPrompts)
+    .then(answers => {
+        let intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+        teamArray.push(intern)
+        return newEmployee();
+    })
+}
+
+const newEmployee = () => {
+    inquirer.prompt([{
         type: 'list', 
-        name: 'next', 
-        message: 'what would you like to do next?',
-        choices: ['add an engineer', 'add an intern', 'finish building my team']
-    };
+        name: 'teamMember', 
+        message: 'what would you like to add next?',
+        choices: [
+            {name: 'engineer', value: 'newEngineer'},
+            {name: 'intern', value: 'newIntern'}, 
+            {name: 'complete', value: 'complete'}
+        ]
+    }])
+    .then( answer => {
+        if (answer.teamMember === 'newEngineer') {
+            engineerQs()
+        } else if (answer.teamMember === 'newIntern') {
+            internQs()
+        }
+
+    })
+}
 
 
-
-// function mainMenu() {
-//     inquirer.prompt(menu)
-//     .then(answer => {
-//         if(answer.next === 'add an engineer') {
-//             newEngineer();
-//         }
-//         else if(answer.next === 'add an intern') {
-//             newIntern();
-//         } else {
-//             console.log('your team is complete! :)')
-//             writetoHTML();
-//         }
-//     })
-// }
-
-// function newEngineer() {
-//     inquirer.prompt(engPrompt)
-//         .then(({name, id, email, github}) => {
-//             var newEngineer = new Engineer(name, id, email, github)
-//             console.log(newEngineer)
-//         })
-//         .then(mainMenu)
-// }
-
-// function newIntern() {
-//     inquirer.prompt(internPrompt)
-//         .then(({name, id, email, school}) => {
-//             var newIntern = new Intern(name, id, email, school)
-//             console.log(newIntern)
-//         })
-//         .then(mainMenu)
-// }
-
-// function writetoHTML() {
-
-// }
-
-// inquirer.prompt(managerPrompt)
-//     .then()
-//     .then(mainMenu)
+start();
