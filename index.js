5d2633d520b32bfecddeaba7603d0674dc7635b0
savoryboi <andbjerk@yahoo.com>
2022-07-21 16:00:19 -0500

    separate classes to new js files in lib folder

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { listenerCount } = require('process');
 
-
-// create arrays of questions for each type of team member
-const managerPrompt = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'enter team manager\'s name:'
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'employee id:'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'email address:'
-    },
-    {
-        type: 'input',
-        name: 'office',
-        message: 'office number:'
-    }
-];
 
 const menu = {
         type: 'list', 
@@ -33,106 +10,13 @@ const menu = {
         choices: ['add an engineer', 'add an intern', 'finish building my team']
     };
 
-const engPrompt = [
-    {
-        type: 'input',
-        name: 'eng_name',
-        message: 'enter engineer\'s name:'
-    },
-    {
-        type: 'input',
-        name: 'eng_id',
-        message: 'engineer\'s id:'
-    },
-    {
-        type: 'input',
-        name: 'eng_email',
-        message: 'email address:'
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'github username:'
-    }
-];
 
-const internPrompt = [
-    {
-        type: 'input',
-        name: 'int_name',
-        message: 'enter intern\'s name:'
-    },
-    {
-        type: 'input',
-        name: 'int_id',
-        message: 'intern\'s id:'
-    },
-    {
-        type: 'input',
-        name: 'int_email',
-        message: 'email address:'
-    },
-    {
-        type: 'input',
-        name: 'school',
-        message: 'school:'
-    } 
-];
-
-class Employee {
-    constructor(name, id, email) {
-        this.name = name;
-        this.id = id;
-        this.email = email;
-    }
-    getName() {
-        
-    }
-    getId() {
-
-    }
-    getEmail() {
-
-    }
-    getRole() {
-
-        return Employee;
-    }
-}
 
 //manager class
-class Manager extends Employee {
-    constructor(name, id, email, officeNumber){
-        super(name, id, email);
-        // this.name = name;
-        // this.id = id;
-        // this.email = email;
-        this.officeNumber = officeNumber;
-    }
-    getRole() {
 
-        return Manager;
-    }
-}
 
 //engineer class 
-class Engineer extends Employee {
-   
-    constructor(name, id, email, github) {
-        super(name, id, email);
-        // this.name = name;
-        // this.id = id;
-        // this.email = email;
-        this.github = github;
-    }
-    getGitHub() {
 
-    }
-    getRole() {
-
-        return Engineer;
-    }
-}
 //intern class
 
 class Intern extends Employee {
@@ -145,15 +29,19 @@ class Intern extends Employee {
         this.school = school;
     }
     getSchool() {
+        inquirer.prompt({
+            name: school,
+            message: 'the intern\'s school:' 
+        })
+        .then(answer => {
 
+        })
     }
     getRole(){
         
-        return Intern;
+        return 'Intern';
     }
 }
-
-
 
 
 
